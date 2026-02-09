4a65a86cab66462288d386916c50624855840aca
anshxpress <anshulgupta19122001@gmail.com>
2026-02-09 15:46:24 +0530

    err fix

--- cubicalseeker/src/LandingPage/Dreamjob.tsx
+++ cubicalseeker/src/LandingPage/Dreamjob.tsx
@@ -24,7 +24,7 @@ return(
         </div>
         <div className="w-[55%] flex  items-center justify-center">
             <div className="w-[30rem] relative">  
-                <img src="/boy.png" alt="boy" />
+                <img src="/Boy.png" alt="boy" />
                 <div className="absolute -right-10 w-fit top-[50%] border-sky-400 border rounded-lg p-2 backdrop-blur-md">
                 <div className="text-center mb-1 text-sm text-mine-shaft-100">5k+ got Job</div>
                 <Avatar.Group>
